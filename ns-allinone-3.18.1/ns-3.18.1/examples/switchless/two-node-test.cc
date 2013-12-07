763b9621479e2ec88c529cf3ce35868e595a7742
Michael McKeown <mmckeown@princeton.edu>
2013-12-06 23:33:05 -0500

    Added more functionality to data-center-app, need to test with more nodes and implement different receivers lists and send patterns.  Added waf script and updated wscript file.

--- ns-allinone-3.18.1/ns-3.18.1/examples/switchless/two-node-test.cc
+++ ns-allinone-3.18.1/ns-3.18.1/examples/switchless/two-node-test.cc
@@ -44,19 +44,38 @@ main (int argc, char * argv[])
 
     Ipv4InterfaceContainer interfaces = address.Assign (devices);
     
-    Ptr<DataCenterApp> app0 = CreateObject<DataCenterApp> ();
-    // TODO: Setup, just want to see if logging is working
+    Ptr<DataCenterApp> app0 = CreateObject<DataCenterApp> (); 
+    DataCenterApp::SendParams app0Params;
+    app0Params.m_sending = true;
+    Ipv4Address app0NodeList[] = {interfaces.GetAddress (1)};
+    app0Params.m_nodes = app0NodeList;
+    app0Params.m_receivers = DataCenterApp::ALL_IN_LIST;
+    app0Params.m_nReceivers = 1;
+    app0Params.m_sendPattern = DataCenterApp::FIXED_INTERVAL;
+    app0Params.m_sendInterval = MilliSeconds (500.);
+    app0Params.m_packetSize = 1024;
+    app0Params.m_nPackets = 10;    
+    app0->Setup(app0Params, true); 
     nodes.Get (0)->AddApplication (app0);
-    app0->SetStartTime (Seconds(1.));
+    app0->SetStartTime (Seconds(0.));
     app0->SetStopTime (Seconds(20.));
 
     Ptr<DataCenterApp> app1 = CreateObject<DataCenterApp> ();
-    // TODO: Setup, just want to see if logging is working
+    DataCenterApp::SendParams app1Params;
+    app1Params.m_sending = true;
+    Ipv4Address app1NodeList[] = {interfaces.GetAddress (0)};
+    app1Params.m_nodes = app1NodeList;
+    app1Params.m_receivers = DataCenterApp::ALL_IN_LIST;
+    app1Params.m_nReceivers = 1;
+    app1Params.m_sendPattern = DataCenterApp::FIXED_INTERVAL;
+    app1Params.m_sendInterval = MilliSeconds (500.);
+    app1Params.m_packetSize = 1024;
+    app1Params.m_nPackets = 10;
+    app1->Setup(app1Params, true);
     nodes.Get (1)->AddApplication (app1);
-    app1->SetStartTime (Seconds(1.));
+    app1->SetStartTime (Seconds(0.));
     app1->SetStopTime (Seconds(20.));
 
-    Simulator::Stop (Seconds (20.));
     Simulator::Run ();
     Simulator::Destroy ();
 
