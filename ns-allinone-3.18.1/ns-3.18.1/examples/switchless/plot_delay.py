a20cf6929b47829b5f051de29d68a20b6e70d17a
Michael McKeown <mmckeown@princeton.edu>
2013-12-11 01:51:01 -0500

    Switching plot_delay.py to plot histograms

--- ns-allinone-3.18.1/ns-3.18.1/examples/switchless/plot_delay.py
+++ ns-allinone-3.18.1/ns-3.18.1/examples/switchless/plot_delay.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python
 
 import sys
-import matplotlib
-matplotlib.use('Agg')
+#import matplotlib
+#matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 import matplotlib.cm as cm
+import matplotlib.mlab as mlab
 import numpy as np
 import re
+import math
 
 def usage () :
     print "Usage: " + sys.argv[0] + " [list of files from parse_output.py]"
@@ -49,20 +51,40 @@ def main () :
                     delayValues[index].append (delay)
                 else :
                     inSizeDelayBlock = False
-                
-    
-    # Make scatter plot
-    colors = cm.rainbow ( np.linspace (0, 1, len (sizeValues)))
-    for i in range(0, len (sizeValues)) :
-        plt.scatter (sizeValues[i], delayValues[i], color=colors[i])
-    #plt.yscale ('log')
-    plt.ylim(ymin=0)
-    plt.xlim(xmin=-10)
+ 
+    # Calculate the max and mean
+    maxDelays = []
+    means = []
+    for i in range (0, len (delayValues)) :
+        sum = 0
+        maxDelay = 0.0
+        for delayValue in delayValues[i] :
+            sum += delayValue
+            if delayValue > maxDelay :
+                maxDelay = delayValue
+        means.append (sum / len (delayValues[i]))
+        maxDelays.append (maxDelay)
+
+    # Calculate standard deviations
+    stdDevs = []
+    for i in range (0, len (delayValues)) :
+        differenceSumsSquared = 0
+        for delayValue in delayValues[i] :
+            difference = delatValue = means[i]
+            differenceSumsSquared += (delayValue * delayValue)
+        stdDevs.append (math.sqrt(differenceSumsSquared / len (delayValues[i])))
+
+    colors = cm.rainbow ( np.linspace (0, 1, len (delayValues)))
+    for i in range (0, len (delayValues)) : 
+        n, bins, patches = plt.hist (delayValues[i], maxDelays[i]/100, color=colors[i], alpha=0.25)
+        y = mlab.normpdf(bins, means[i], stdDevs[i])
+        plt.plot(bins, y, color=colors[i])
+ 
     plt.legend (resultFilenames)
-    plt.xlabel ("Packet Size (bytes)")
-    plt.ylabel ("Packet Delay (ns) (Log Scale)")
-    plt.savefig ("delay.pdf")
-    
+    plt.xlabel ("Packet Delay (ns)")
+    plt.ylabel ("Occurrencces")
+    plt.show()
+
 
 if __name__ == "__main__" :
     main()
