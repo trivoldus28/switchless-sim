b57380d563e946bb61c115755ce3f91109943c99
Michael McKeown <mmckeown@princeton.edu>
2014-01-14 20:19:12 -0500

    Fixed nbody delay graph and updated plot_latency.py

--- ns-allinone-3.18.1/ns-3.18.1/examples/switchless/plot_latency.py
+++ ns-allinone-3.18.1/ns-3.18.1/examples/switchless/plot_latency.py
@@ -4,6 +4,7 @@ import sys
 import matplotlib
 matplotlib.use('Agg')
 import matplotlib.pyplot as plt
+import matplotlib.cm as cm
 import numpy as np
 
 def usage () :
@@ -28,23 +29,27 @@ def plotLatency(resultFilenames):
         foundLatency = False
         for line in resultFile :
             if "Latency: " in line :
-                latencies.append (float(line[9:-1]))
+                latencies.append (float(line[9:-1])/1000.0)
                 foundLatency = True
                 break
         
         if not foundLatency :
             print "Latency value not found in file '" + resultFilename + "'"
             sys.exit(1)
+
+    matplotlib.rcParams.update({'font.size':17})
+    matplotlib.rcParams.update({'figure.autolayout': True})
     
     # Plot latencies
     x = np.arange (len (latencies))
-    plt.bar (x, latencies)
+    plt.bar (x + 0.5, latencies, color='gray', width=0.3)
     shortenedfilenames = []
     for name in resultFilenames:
         shortenedfilenames.append(name.split('.')[0])
-    plt.xticks (x + 0.4, shortenedfilenames)
-    plt.ylabel ("Latency (ns)")
-    plt.savefig ("latency.pdf")
+    plt.xticks (x + 0.65, shortenedfilenames, rotation=45, ha='right')
+    plt.xlim(xmin=0)
+    plt.ylabel ("Latency (us)")
+    plt.savefig ("latency.pdf", cmap=cm.Greys_r)
     plt.close()
     
 def main () :
